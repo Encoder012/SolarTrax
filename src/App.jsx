91a881f5fa6cfcf397b7b4a9cb56331313402d78
Encoder012 <49637067+Encoder012@users.noreply.github.com>
2024-10-26 04:10:32 +0530

    updated home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
+import Home from './pages/Home';
 import Projects from './pages/Projects';
 import PostProject from './pages/PostProject';
 
