fbdfdb7399d1152c0e46d601b0f2b5e0bd62a0d8
Mukul <varun.gupta8851401313@gmail.com>
2024-10-26 04:07:28 +0530

    pushing the changes by mukul

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import Projects from './pages/Projects';
+import PostProject from './pages/PostProject';
+import { SignUp } from './pages/SignUp';
+import { Login } from './pages/login';
 
 function App() {
 
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Home/>} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/post-project" element={<PostProject/>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   </div>
