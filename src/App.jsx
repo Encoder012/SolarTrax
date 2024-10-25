a2b53a1ba496e672749f287e232adcb87a5e6f33
Om Sharma <omsharma050322@gmail.com>
2024-10-25 19:07:31 +0000

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Home from './pages/home'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import Projects from './pages/Projects';
 
 function App() {
 
 
-  return (<>
-   <Home />
-  </>
+  return (<div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  </div>
   
   )
 }
