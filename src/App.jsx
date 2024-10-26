0f1ef40b7a0974564eed4d4f699f0dac1c1ca6c4
Mukul <varun.gupta8851401313@gmail.com>
2024-10-26 11:46:12 +0530

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 
 import './App.css'
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import PostProject from './pages/PostProject';
 import { SignUp } from './pages/SignUp';
 import { Login } from './pages/login';
+import Navbar from './components/navbar';
 
 
 import { Dashboard } from './components/dashboard';
+import Donation from './pages/Donation';
 function App() {
 
 
   return (<div>
+    
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' exact element={<Home/>} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/post-project" element={<PostProject/>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />        <Route path="/projects/post-project" element={<PostProject />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects/post-project" element={<PostProject />} />
+        <Route path="/donation" element={<Donation />} />
       
-        
-
       </Routes>
-    </Router>
+      </Router>
   </div>
   
   )
